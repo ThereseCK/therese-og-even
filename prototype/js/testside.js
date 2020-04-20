e782d11b0472c6d96ec775eda10d5d53287ad7f5
therese camilla nordnes <59839349+ThereseCK@users.noreply.github.com>
2020-04-20 10:45:21 +0200

    ,

---
+++ prototype/js/testside.js
@@ -0,0 +1,75 @@
+
+// console.log(model.users.map(n => n.name).join(' + '))
+
+
+// document.write(model.selectCourse.yoga
+//     .map(n => n.info + ' ' + n.name +' ' + n.maxParticipants +' ' + n.price +' ' + n.currentParticipants)
+
+// .join(' <br>'))
+//if(model.login.usernameEntry == 'even'){ model.login.isAdmin =true}  i en login controller :D
+
+
+// function selectModuleAndShow(selection) {
+//     model.selectedModule = selection;
+   
+// }
+
+// function show() {
+//     let showInfoHTML= `
+//    
+// content.innerHTML = showInfoHTML;
+        
+// }     
+
+// let test = model.filter(n => n.month === 'Mars')
+
+
+
+{/* <h2>Kategorier</h2>
+<ul>
+    ${categories.map(cat => `
+    <li>${cat.name} <button onclick="selectCategory(${cat.id})">velg</button></li>    
+    `).join('')}
+</ul>            
+<h2>Kurs</h2>
+<ul>
+    ${courses.map(course => `
+    <li>${course.name} <button onclick="register(${course.id})">velg</button></li>    
+    `).join('')}
+</ul>           
+
+
+*/}
+// const courses = model.calender.courses.filter(cat => cat.category === categoryId);
+// document.getElementById('content').innerHTML = `
+// <select>
+// ${categories.map(cat => `
+// ${cat.name} <option onclick="selectCategory()">${cat.name}</option>    
+// `).join('')}
+//     </select> <br>
+//     <select> 
+//     ${courses.map(course => `
+// ${course.name} <option onclick="register()">${course.id}</option>    
+// `).join('')}
+//     </select><br>
+
+
+
+/* <select>
+${model.selectCourse.yoga.map(k => `<option>${k.name}</option>`).join(' ')}
+</select> <br>
+<select> 
+${model.selectCourse.events.map(l => `<option>${l.name}</option>`).join(' ')}
+</select><br>
+<select>
+${model.selectCourse.sessions.map(v => `<option>${v.name}</option>`).join(' ')}
+ </select> */
+
+
+ 
+ 
+// const u = model.menuOptions.map(i => i.onlyForAdmin)  ? 'Admin' : 'Bruker';
+// //model.logInSession;
+// console.log(u)
+
+// model.categories[model.selectedCategoryId-1].info == null  ? test = ''  : test = model.categories[model.selectedCategoryId-1].info;
